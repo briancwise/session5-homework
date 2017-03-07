61fabcd6279c9359435d81eb2378ac35326623b0
Brian Wise <brianwisenyc@gmail.com>
2017-03-07 16:33:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const bodyParser = require('body-parser')
 const app = express() 
 const port = 9000
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 // app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}!`)
 })
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html')
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname + '/index.html')
+// })
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 
